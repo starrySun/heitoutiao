f6868d82d685477211a64b89be365949aa72df53
nick <455105593@qq.com>
2019-08-13 10:16:02 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 所有文件引入包的入口
+// 引用element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
